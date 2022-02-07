32122f22c940bfbcac29e39b2b43b6afa4ec3264
Pavel Vasilev <pv9998286620@yandex.ru>
2022-02-07 10:21:01 +0300

    first commit

---
+++ src/compenents/Counter.tsx
@@ -0,0 +1,25 @@
+import React, {useState} from 'react';
+import {CounterPanel} from './Counter/CounterPanel';
+import {CounterButtons} from './Counter/CounterButtons';
+
+export const Counter = () => {
+    const [value, setValue] = useState<number>(0)
+
+    const error = (value >= 5) ? true : false
+    const errorButtonReset = (value == 0) ? true : false
+
+    return (
+        <div className={'counter__wrapper'}>
+            <div className={'counter'}>
+                <CounterPanel value={value} error={error}/>
+                <CounterButtons
+                    value={value}
+                    setValue={setValue}
+                    error={error}
+                    errorButtonReset={errorButtonReset}
+                />
+            </div>
+        </div>
+    );
+};
+
